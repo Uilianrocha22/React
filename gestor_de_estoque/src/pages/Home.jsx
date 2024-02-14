3c41ea928cfd5aa49b2db2b240a24bd185734b1f
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-02-14 14:38:40 -0300

    implementação do component Home pagina Dashiboard

--- gestor_de_estoque/src/pages/Home.jsx
+++ gestor_de_estoque/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import useStock from "../hooks/useStock";
+
 export default function Home() {
+  const { items } = useStock();
+
+  const diversity = items.length;
+  const inventoryTotal = items.reduce((sum, item) => +sum + +item.quantity, 0);
+
   return (
     <main>
       <h1>Dashiboard</h1>
+      <div className="row">
+        <div className="dashboard-card">
+          Diversidade de itens
+          <span>{diversity}</span>
+        </div>
+        <div className="dashboard-card">
+          Inventario total
+          <span>{inventoryTotal}</span>
+        </div>
+      </div>
     </main>
   );
 }
