ab20e653a25e97e3dcb3945f6364e48a369364f9
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-02-15 18:28:42 -0300

    finalização do component Home, Itens recentes e Itens acabando

--- gestor_de_estoque/src/pages/Home.jsx
+++ gestor_de_estoque/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useStock from "../hooks/useStock";
 
 export default function Home() {
@@ -5,6 +6,17 @@ export default function Home() {
 
   const diversity = items.length;
   const inventoryTotal = items.reduce((sum, item) => +sum + +item.quantity, 0);
+
+  const today = new Date();
+  const limitDate = new Date();
+  limitDate.setDate(limitDate.getDate() - 10);
+  const recentItems = items.filter(
+    (item) => item.createdAt >= limitDate && item.createdAt <= today
+  );
+  const recentTotal = recentItems.length;
+
+  const lowQuantityItems = items.filter((item) => item.quantity < 10);
+  const lowQuantityTotal = lowQuantityItems.length;
 
   return (
     <main>
@@ -14,9 +26,68 @@ export default function Home() {
           Diversidade de itens
           <span>{diversity}</span>
         </div>
+
         <div className="dashboard-card">
           Inventario total
           <span>{inventoryTotal}</span>
+        </div>
+
+        <div className="dashboard-card">
+          Itens recentes
+          <span>{recentTotal}</span>
+        </div>
+
+        <div className="dashboard-card">
+          Itens acabando
+          <span>{lowQuantityTotal}</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="recent">
+          <table>
+            <thead>
+              <tr>
+                <th>Itens Recentes</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {recentItems.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>
+                    <Link to={`/items/${item.id}`} className="button is-small">
+                      Ver
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="low">
+          <table>
+            <thead>
+              <tr>
+                <th>Itens acabando</th>
+                <th>Qtd.</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {lowQuantityItems.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <Link to={`/items/${item.id}`} className="button is-small">
+                      Ver
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </main>
